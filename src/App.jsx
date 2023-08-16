53a9ed7e111d207cc53a2e139762c841b3b222a3
Elvis <elvisorlando.dev@gmail.com>
2023-08-16 16:52:31 +0200

    feature: Route Layout implementation with outlet

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,22 @@ import Home from './pages/Home/Home'
 import About from "./pages/About/About"
 import Vans from "./pages/Vans/Vans"
 import VanDetail from "./pages/VanDetail/VanDetail"
-import Navbar from "./components/Navbar/Navbar"
-import Footer from './components/Footer/Footer'
+import Layout from "./components/Layout"
 import './server'
 
 function App() {
   return (
     <>
       <BrowserRouter>
-          <Navbar />
-
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/vans" element={<Vans />} />
-            <Route path="/vans/:id" element={<VanDetail />} />
+            <Route element={<Layout />}>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/vans" element={<Vans />} />
+              <Route path="/vans/:id" element={<VanDetail />} />
+            </Route>
           </Routes>
       </BrowserRouter>
-
-      <Footer />
     </>
   )
 }
